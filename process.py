1b24c1568e929b2862236bce14c9935b3f4e2210
VictorEscribano <vesgarcia99@gmail.com>
2023-08-07 16:43:41 +0200

    Organize by functions

--- process.py
+++ process.py
@@ -3,13 +3,17 @@ from Chatbot import *
 import shutil
 
 api_token = "r8_Cb06Mjr3LNnlc0Cnamkp6N4sW0utmwF4ShBOK"
+full_path = r'C:\Users\vesga\Documentos\Victor\Codin_projects\AutoTikTok'
 
+Description_personality = "You reply by the name of Assistant. You will reply directly with an answer. You are a very clickbait person that wants to go viral. You love to add Hashtacks."
+search_text = 'aliens ufo UAV US government'
+Description = f'Like and subscribe for more videos! {search_text}'
 
 def search_and_download(bot, search_text, scroll=8):
     time.sleep(5)
     bot.search(search_text)
     time.sleep(3)
-    bot.scroll(8)
+    bot.scroll(scroll)
 
     urls = bot.GetURLs()
     print(urls)
@@ -21,45 +25,33 @@ def search_and_download(bot, search_text, scroll=8):
     time.sleep(5)
     pass
 
+def upload_video(bot, video, Description):
+    if Description == None:
+        print('Error generating description')
+        Description = 'Like and subscribe for more videos! '
+    else:
+        print(f'Description: {Description}')
 
-if __name__ == "__main__":
-    Description_personality = "You reply by the name of Assistant. You will reply directly with an answer. You are a very clickbait person that wants to go viral. You love to add Hashtacks."
-    
-    Description_bot = LLMChatBot(api_token)
-
-    bot = tiktokBot(window=True)
-    bot.login(interval=0.07)
-    
-    search_text = '#aliens'
-    # search_and_download(bot, search_text, scroll=2)
-
-    print('Entering upload loop...')
-    time.sleep(6)
-    full_path = r'C:\Users\vesga\Documentos\Victor\Codin_projects\AutoTikTok'
-    for video in glob.glob('videos/*.mp4'):
-        Description_prompt_input = f"Write a very realistic Tik-Tok video description for your last video and a lot of hashtacks about this topic: {video}. It needs to go viral so add a lot of hashtaks related. Assistant:"
-        try:
-            Description = Description_bot.generate_response(Description_personality, 
-                                                        Description_prompt_input)
-        except:
-            print('Error generating description')
-            Description = f'Like and subscribe for more videos! {search_text}'
-        
-        time.sleep(5)
-        if Description == None:
-            print('Error generating description')
-            Description = 'Like and subscribe for more videos! '
-        else:
-            print(f'Description: {Description}')
-
-        try:
-            bot.upload2TikTok(f'{full_path}\{video}', f'{Description} ')
-        except:
-            print('Error uploading video')
-            bot.goToNewTab()
-            continue
-
+    try:
+        bot.upload2TikTok(f'{full_path}\{video}', f'{Description} ')
         time.sleep(1)
-        #move the video to the Used_videos folder
         shutil.move(video, 'Used_videos')
         time.sleep(1)
+    except Exception as e:
+        print(f'Error uploading video: {e}')
+        bot.goToNewTab()
+
+if __name__ == "__main__":
+    
+    Description_bot = LLMChatBot(api_token)
+    bot = tiktokBot(window=True, proxy=False)
+
+    bot.login(interval=0.07)
+    search_and_download(bot, search_text, scroll=2)
+
+    print('Entering upload loop...')
+    for video in glob.glob('videos/*.mp4'):
+        
+        upload_video(bot, video, Description)
+
+        
